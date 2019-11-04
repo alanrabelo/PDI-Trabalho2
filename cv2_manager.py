89443d746ab2f96a553665642c001c762b868e8f
alan <alanrabelo13@gmail.com>
2019-11-04 13:45:53 -0300

    Adding more operations Adding new images

--- cv2_manager.py
+++ cv2_manager.py
@@ -2,7 +2,9 @@
 import numpy as np
 import cv2
 from skimage.io import imread, imsave
-
+import math
+import matplotlib.pyplot as plt
+import pywt
 
 class CV2Manager:
 
@@ -34,12 +36,109 @@ class CV2Manager:
         rotacionado1 = cv2.warpAffine(self.img, rotacao1, (largura, altura))
         imsave(self.plot_folder+'rotation-45-tamanho-'+self.filename, rotacionado1)
 
+    def houghCircles(self, plot_folder='Plots/Hough/'):
+        img = self.img
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
+                                   param1=50, param2=30, minRadius=0, maxRadius=40)
+
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+        imsave(plot_folder+'hough-circles'+self.filename, cimg)
+
+    def houghLines(self, plot_folder='Plots/Hough/'):
+        dst = cv2.Canny(self.img, 50, 200, None, 3)
+
+        # Copy edges to the images that will display the results in BGR
+        cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+        lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
+
+        if lines is not None:
+            for i in range(0, len(lines)):
+                rho = lines[i][0][0]
+                theta = lines[i][0][1]
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+                pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+                cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+
+        imsave(plot_folder+'hough-lines'+self.filename, cdst)
+
+    def wavelet(self, plot_folder='Plots/Hough/'):
+        img_haar = pywt.dwt2(self.img, 'haar')
+        cA, (cH, cV, cD) = img_haar
+
+        plt.figure(figsize=(9,9))
+
+        plt.subplot(221)
+        plt.imshow(cA, 'gray')
+        plt.title('Original')
+        plt.subplot(222)
+        plt.imshow(cH, 'gray')
+        plt.title('Horizontais')
+        plt.subplot(223)
+        plt.imshow(cV, 'gray')
+        plt.title('Verticais')
+        plt.subplot(224)
+        plt.imshow(cD, 'gray')
+        plt.title('Diagonais')
+        plt.show()
+        plt.savefig(plot_folder+'wavelet'+self.filename)
+
+    def plotResult(self, result, title, folder):
+        fig = plt.figure(figsize=(8, 3.5), dpi=150)
+        a = fig.add_subplot(1,2,1)
+        a.axis('off')
+        plt.imshow(self.img, cmap=plt.get_cmap('gray'))
+        a.set_title('Original')
+
+        a = fig.add_subplot(1,2,2)
+        a.axis('off')
+        plt.imshow(result, cmap=plt.get_cmap('gray'))
+        a.set_title(title)
+
+        plt.savefig(folder+title+'-'+self.filename)
+
+    def fourier(self, plot_folder='Plots/Fourier/'):
+        img = self.img
+        rows, cols = img.shape
+        img_dft = np.fft.fft2(img)
+        img_dft_shift = np.fft.fftshift(img_dft)
+        img_dft_mag = np.abs(img_dft_shift)
+
+        self.plotResult(np.log(img_dft_mag), 'Espectro em frequência', plot_folder)
+
+        img_idft = np.fft.ifft2(img_dft)
+        img_inversa = np.abs(img_idft)
+
+        self.plotResult(img_inversa, 'Imagem após IDFT', plot_folder)
+
+        # cv2.imwrite('./Results/{}-kmeans.png'.format(self.name_file), result)
+
+
 images = [
     'apple-1.gif',
     'deer-16.gif',
 ]
+gs_images = [
+    'lena_gray_512.tif',
+    'woman_blonde.tif',
+]
 
-manager = CV2Manager()
-for filename in images:
+manager = CV2Manager(files_folder='Datasets/GrayScale/')
+for filename in gs_images:
     manager.load_image(filename)
-    manager.rotate_and_translate_image()
+    # manager.rotate_and_translate_image()
+    # manager.houghCircles()
+    # manager.houghLines()
+    # manager.rotate_and_translate_image()
+    manager.fourier()
--- morphological_operators.py
+++ morphological_operators.py
@@ -8,7 +8,10 @@ from skimage.io import imread, imsave
 # FUNÇÕES MORFOLOGICAS
 from skimage import morphology
 from skimage.morphology import square, rectangle, disk
-
+from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+import cv2
+import numpy as np
 
 class MorphologyManager:
 
@@ -133,6 +136,78 @@ class MorphologyManager:
         plt.tight_layout()
         plt.savefig(plot_folder +'morphological-gradient-' + self.filename)
 
+    def kmeans(self, number_of_clusters=5, plot_folder="Plots/7-K-means-clustering/"):
+
+        (h1, w1) = self.img.shape[:2]
+
+        image = self.img.reshape((h1 * w1, 1))
+        final_images = []
+
+        for i in range(2, 7):
+
+            clt = KMeans(n_clusters=i)
+
+            labels = clt.fit_predict(image)
+            quant = clt.cluster_centers_.astype("uint8")[labels]
+
+            quant = quant.reshape((h1, w1))
+            final_images.append(quant)
+
+        fig = plt.figure(figsize=(10, 8))
+        a = fig.add_subplot(2, 3, 1)
+        plt.imshow(self.img, cmap=plt.cm.gray)
+        a.set_title('Original')
+        plt.axis('off')
+
+        a = fig.add_subplot(2, 3, 2)
+        plt.imshow(final_images[0], cmap=plt.cm.gray)
+        a.set_title('k = 2')
+        plt.axis('off')
+
+        a = fig.add_subplot(2, 3, 3)
+        plt.imshow(final_images[1], cmap=plt.cm.gray)
+        a.set_title('k = 3')
+        plt.axis('off')
+
+        a = fig.add_subplot(2, 3, 4)
+        plt.imshow(final_images[2], cmap=plt.cm.gray)
+        a.set_title('k = 4')
+        plt.axis('off')
+
+        a = fig.add_subplot(2, 3, 5)
+        plt.imshow(final_images[3], cmap=plt.cm.gray)
+        a.set_title('k = 5')
+        plt.axis('off')
+
+        a = fig.add_subplot(2, 3, 6)
+        plt.imshow(final_images[4], cmap=plt.cm.gray)
+        a.set_title('k = 6')
+        plt.axis('off')
+
+        plt.tight_layout()
+        plt.savefig(plot_folder + 'k-means-' + self.filename)
+
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+    def detect_circles(self):
+
+        img = cv2.medianBlur(self.img, 5)
+        cimg = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+
+        circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1, 20,
+                                   param1=100, param2=120, minRadius=0, maxRadius=0)
+
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+        cv2.imshow('detected circles', cimg)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 import glob
 bw_images = [
@@ -140,9 +215,12 @@ bw_images = [
     'apple-1.gif',
 ]
 
+# gs_images = [
+#     'lena_gray_512.tif',
+#     'woman_blonde.tif',
+# ]
 gs_images = [
-    'lena_gray_512.tif',
-    'woman_blonde.tif',
+    'coins.tif',
 ]
 
 # manager = MorphologyManager()
@@ -170,5 +248,8 @@ for filename in gs_images:
     manager.load_images()
     # manager.dilate_image(structuring_elements)
     # manager.erode_image(structuring_elements)
-    manager.morphological_gradient()
-
+    # manager.morphological_gradient()
+    #
+    # for i in range(2, 6):
+    #     manager.kmeans(i)
+    manager.detect_circles()
